b43e4666bca1e01a3bde28199e47a1e4bccfd0ba
thenithinbalaji <thenithinbalaji@gmail.com>
2023-02-11 01:12:53 +0530

    fixed navbar link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import logo from "../assets/airbnb_logo.svg"
 export default function NavBar() {
     return (
         <div className="shadow-md md:pl-16 py-5 flex fixed w-full bg-white z-40">
-            <a href="/" className="w-3/12 sm:w-2/12 md:w-1/12 mx-auto md:mx-0">
+            <a href={process.env.PUBLIC_URL + "/"} className="w-3/12 sm:w-2/12 md:w-1/12 mx-auto md:mx-0">
                 <img src={logo} alt="airbnb logo" className="w-full" />
             </a>
         </div>
